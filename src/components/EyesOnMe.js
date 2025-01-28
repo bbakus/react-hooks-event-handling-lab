0fb3ee13a6ed0686e5c76aa8952d236fbca5fc0e
Brandon Bakus <brandonbakus@gmail.com>
2025-01-28 16:39:50 -0500

    sharp-soft

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,11 @@
 // Code EyesOnMe Component Here
+
+function EyesOnMe(){
+    const sharp = () => console.log('Good!')
+    const soft = () => console.log('Hey! Eyes on me!')
+    return(
+        <button type="text" onFocus={sharp} onBlur={soft} placeholder="Eyes on me">Eyes on me</button>
+    )
+}
+
+export default EyesOnMe
